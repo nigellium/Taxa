9c2b381542b9583a6bf9a4e61135fc9fd0407d6b
czv8 <czv8@cdc.gov>
2018-10-04 10:35:57 -0400

    Add files via upload

--- src/main/java/gov/cdc/taxonomy/util/TaxonomyFileParser.java
+++ src/main/java/gov/cdc/taxonomy/util/TaxonomyFileParser.java
@@ -15,80 +15,76 @@ import gov.cdc.taxonomy.model.ChildTaxonomyNode;
 import gov.cdc.taxonomy.model.TaxonomyNode;
 
 public class TaxonomyFileParser {
-	
+
 	/**
-	 * Parse the taxonomy file into 
+	 * Parse the taxonomy file into
 	 */
 	public static TaxonomyNode parse(File[] files, String nodeId) throws IOException {
-		Optional <String> node = Files.newBufferedReader(Paths.get(files[0].getAbsolutePath())).
-				lines().filter(s->s.trim().startsWith(nodeId)).findFirst();
+		Optional<String> node = Files.newBufferedReader(Paths.get(files[0].getAbsolutePath())).lines()
+				.filter(s -> s.trim().startsWith(nodeId)).findFirst();
 		if (node.isPresent()) {
 			TaxonomyNode nd = new TaxonomyNode(node.get());
-			//now parse the second file for descendents
+			// now parse the second file for descendents
 			final List<Authority> authorities = new ArrayList<>();
-			Files.newBufferedReader(Paths.get(files[1].getAbsolutePath())).
-			lines().filter(s -> s.trim().startsWith(nodeId)).forEach( s -> {
-				authorities.add( new Authority(s));
-			});
+			Files.newBufferedReader(Paths.get(files[1].getAbsolutePath())).lines()
+					.filter(s -> s.trim().startsWith(nodeId)).forEach(s -> {
+						authorities.add(new Authority(s));
+					});
 			if (!authorities.isEmpty()) {
 				nd.setAuthorities(authorities);
 			}
 			return nd;
 		}
-		return null;	
+		return null;
 	}
-	
+
 	/**
 	 * add a new entry to node file, and two entries to name file
-	 * @param files array of two files, node file and name file
+	 * 
+	 * @param files
+	 *            array of two files, node file and name file
 	 * @param parentId
 	 * @param rank
 	 * @param label
 	 * @throws IOException
 	 */
 	public static void addNode(File[] files, String parentId, String rank, String label) throws IOException {
-		
+
 		final List<Integer> listNdId = new ArrayList<>();
 		listNdId.add(0);
-		
-		
-		Files.newBufferedReader(Paths.get(files[0].getAbsolutePath())).lines().forEach(s-> {
+
+		Files.newBufferedReader(Paths.get(files[0].getAbsolutePath())).lines().forEach(s -> {
 			int ndId = listNdId.get(0);
 			String[] arr = s.split("\\|");
-			int pId = new Integer(arr[1].trim());
-			int taxId = new Integer(arr[0].trim());
-			if (pId > ndId) {
-				ndId = pId;
+			if (arr.length > 1) {
+				int pId = new Integer(arr[1].trim());
+				int taxId = new Integer(arr[0].trim());
+				if (pId > ndId) {
+					ndId = pId;
+				}
+				if (taxId > ndId) {
+					ndId = taxId;
+				}
+				listNdId.add(0, ndId);
 			}
-			if (taxId > ndId) {
-				ndId = taxId;
-			}
-			listNdId.add(0, ndId);
 		});
-		
-		String strNode = createNodeEntry (listNdId.get(0), parentId, rank);
+
+		String strNode = createNodeEntry(listNdId.get(0), parentId, rank);
 		String strName = createNameEntry(listNdId.get(0), label);
-		
-		Files.write(
-			      Paths.get(files[0].getAbsolutePath()), 
-			      strNode.getBytes(), 
-			      StandardOpenOption.APPEND);
-		
-		Files.write(
-			      Paths.get(files[1].getAbsolutePath()), 
-			      strName.getBytes(), 
-			      StandardOpenOption.APPEND);
-		
+
+		Files.write(Paths.get(files[0].getAbsolutePath()), strNode.getBytes(), StandardOpenOption.APPEND);
+
+		Files.write(Paths.get(files[1].getAbsolutePath()), strName.getBytes(), StandardOpenOption.APPEND);
+
 	}
 
 	private static String createNameEntry(Integer ndId, String label) {
-		return "\r\n" + ndId + "|" + label + "||scientific name|\r\n" +
-		ndId + " | user supplied | | authority|";
+		return ndId + "|" + label + "||scientific name|\r\n" + ndId + " | user supplied | | authority|\r\n";
 
 	}
 
 	private static String createNodeEntry(Integer ndId, String parentId, String rank) {
-		return "\r\n" + ndId + "|" + parentId + "|" + rank + "||||";
+		return ndId + "|" + parentId + "|" + rank + "||||\\r\\n";
 	}
 
 }
