fcc82ea0affe5eb41f18cbb1279535d5b267c5b8
Pasupathi <swp4@cdc.gov>
2018-10-03 22:40:28 -0400

    modified the controller

--- src/main/java/gov/cdc/taxonomy/rest/BasicRestController.java
+++ src/main/java/gov/cdc/taxonomy/rest/BasicRestController.java
@@ -1,10 +1,21 @@
 package gov.cdc.taxonomy.rest;
 
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +34,10 @@ public class BasicRestController {
 	@RequestMapping("/")
 	  public String home() {
 	    return "index.html";
+	  }
+	@RequestMapping("/add")
+	  public String add() {
+	    return "add.html";
 	  }
 	@RequestMapping(value = "/query", method = RequestMethod.POST)
 	
@@ -46,5 +61,28 @@ public class BasicRestController {
         }
 	    //return "Hello World";
 	  }
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ResponseEntity<?> generateZip(@RequestParam("nodeFile") MultipartFile nodeFile,@RequestParam("nameFile") MultipartFile nameFile, @RequestParam("term")String term) throws IOException {
+		 HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.parseMediaType("application/zip"));
+	        String outputFilename = "output.zip";
+	        headers.setContentDispositionFormData(outputFilename, outputFilename);
+	        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+
+	        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+	        ZipOutputStream zipOutputStream = new ZipOutputStream(byteOutputStream);
+	        File [] files = new File[] {new File(nodeFile.getOriginalFilename()), new File(nameFile.getOriginalFilename())};
+        	
+
+	        for(File file: files) {
+	            zipOutputStream.putNextEntry(new ZipEntry(file.getName()));           
+	            FileInputStream fileInputStream = new FileInputStream(file);
+	            IOUtils.copy(fileInputStream, zipOutputStream);
+	            fileInputStream.close();
+	            zipOutputStream.closeEntry();
+	        }           
+	        zipOutputStream.close();
+	    return new ResponseEntity<>(byteOutputStream.toByteArray(), headers, HttpStatus.OK); 
+	}
 
 }
