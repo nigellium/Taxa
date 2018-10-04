6ed27a4f9d4a6fe7f07e47a17a23d92d7e7913ba
czv8 <czv8@cdc.gov>
2018-10-04 10:19:32 -0400

    Add files via upload

--- src/main/java/gov/cdc/taxonomy/util/TaxonomyFileParser.java
+++ src/main/java/gov/cdc/taxonomy/util/TaxonomyFileParser.java
@@ -1,9 +1,11 @@
+
 package gov.cdc.taxonomy.util;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -34,6 +36,58 @@ public class TaxonomyFileParser {
 			return nd;
 		}
 		return null;	
+	}
+	
+	/**
+	 * add a new entry to node file, and two entries to name file
+	 * @param files array of two files, node file and name file
+	 * @param parentId
+	 * @param rank
+	 * @param label
+	 * @throws IOException
+	 */
+	public static void addNode(File[] files, String parentId, String rank, String label) throws IOException {
+		
+		final List<Integer> listNdId = new ArrayList<>();
+		listNdId.add(0);
+		
+		
+		Files.newBufferedReader(Paths.get(files[0].getAbsolutePath())).lines().forEach(s-> {
+			int ndId = listNdId.get(0);
+			String[] arr = s.split("\\|");
+			int pId = new Integer(arr[1].trim());
+			int taxId = new Integer(arr[0].trim());
+			if (pId > ndId) {
+				ndId = pId;
+			}
+			if (taxId > ndId) {
+				ndId = taxId;
+			}
+		});
+		
+		String strNode = createNodeEntry (listNdId.get(0), parentId, rank);
+		String strName = createNameEntry(listNdId.get(0), label);
+		
+		Files.write(
+			      Paths.get(files[0].getAbsolutePath()), 
+			      strNode.getBytes(), 
+			      StandardOpenOption.APPEND);
+		
+		Files.write(
+			      Paths.get(files[1].getAbsolutePath()), 
+			      strName.getBytes(), 
+			      StandardOpenOption.APPEND);
+		
+	}
+
+	private static String createNameEntry(Integer ndId, String label) {
+		return "\r\n" + ndId + "|" + label + "||scientific name|\r\n" +
+		ndId + " | user supplied | | authority|";
+
+	}
+
+	private static String createNodeEntry(Integer ndId, String parentId, String rank) {
+		return "\r\n" + ndId + "|" + parentId + "|" + rank + "||||";
 	}
 
 }
